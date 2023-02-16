c98a461cee43b2016beb04da6682f512030f57cd
Acekicker277 <oyetolaayo@gmail.com>
2023-02-16 14:48:24 -0800

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  *  * main - print the phrase "Programming is like building a multilingual puzzle
  *   * Return: 0 of exited properly, non-zero otherwise
- *    */
+*/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
