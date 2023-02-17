3dcf69292a9698528ce7c89d5429840d000bf003
Acekicker277 <oyetolaayo@gmail.com>
2023-02-17 07:06:28 -0800

    alx-low_level_programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,21 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * main - Determines if an integer is positive, negative or zero
- * Return:always 0 (Success)
-*/
+ *  * main - Determines if an integer is positive, negative or zero
+ *   * Return:always 0 (Success)
+ *    */
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-					if (n < 0)
-								printf("%d is negative\n", n);
-						else if (n > 0)
-									printf("%d is positive\n", n);
-							else
-										printf("%d is zero\n", n);
-								return (0);
+		srand(time(0));
+			n = rand() - RAND_MAX / 2;
+				/* your code goes there */
+				if (n < 0)
+							printf("%d is negative\n", n);
+	else if (n > 0)
+								printf("%d is positive\n", n);
+       else 
+              printf("%d is zero\n", n);
+       return (0);
 }
