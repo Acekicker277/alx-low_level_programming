b17ce9d12f48310b4e9c72471d5fef2e7e0b0b64
Acekicker277 <oyetolaayo@gmail.com>
2023-02-19 07:50:55 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,27 +10,27 @@ int main(void)
 	int a = 0;
 	int b;
 
-	while (a <= 98) /*run following while a less than or equal to 98*/
+	while (b <= 99) /*run following until b equal to 99*/
 	{
-		while (a <= 98) /*run following while a less than or equal to 98*/
-			while (b <= 99) /*run following until b equal to 99*/
+		b = a + 1; /*b is always one more than a*/
+		while (b <= 99) /*run following until b equal to 99*/
+		{
+			putchar(a / 10 % 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 % 10 + '0');
+			putchar(b % 10 + '0');
+			if (a == 98 && b == 99)
 			{
-				putchar(a / 10 % 10 + '0');
-				putchar(a % 10 + '0');
-				putchar(' ');
-				putchar(b / 10 % 10 + '0');
-				putchar(b % 10 + '0');
-				if (a == 98 && b == 99)
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				b++;
+				putchar('\n');
 			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
 		a++; /*increment this loop by 1*/
 	}
 	return (0);
