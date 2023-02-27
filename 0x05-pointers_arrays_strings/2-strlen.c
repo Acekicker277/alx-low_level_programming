d90aab807aa3d23ec717b1a7b32de44f7c3a92d9
Acekicker277 <oyetolaayo@gmail.com>
2023-02-27 09:34:09 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- *  * _strlen - returns the length of a string.
- *   * @s: input string.
- *    * Return: length of a string.
- *     */
+ * _strlen - return the length of the string
+ * @s: char type pointer
+ * Return: length of string
+ i*/
+
 int _strlen(char *s)
 {
-		int count = 0;
+	int Acekicker;
 
-			while (*(s + count) != '\0')
-						count++;
-				return (count);
+	for (Acekicker = 0; s[Acekicker] != '\0'; Acekicker++)
+		;
+
+	return (0);
 }
