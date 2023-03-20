218b461b86d462279d4c44ee584dca20774ea4f4
Acekicker277 <oyetolaayo@gmail.com>
2023-03-19 17:12:46 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,19 +9,19 @@
 
 int main(int argc, char *argv[])
 {
-		int mul, num1, num2;
+	int mul, num1, num2;
 
-			if (argc < 2 || argc > 3)
-					{
-								printf("Error\n");
-										exit(98);
-											}
+	if (argc < 2 || argc > 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 
-				num1 = atoi(argv[1]);
-					num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-						mul = num1 * num2;
-							printf("%d\n", mul);
-								return (0);
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
 }
